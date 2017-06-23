ed3e19704f3f9e5344ff419b560e407bb64bb46f
Katherine Lynch <katherly@upenn.edu>
2017-06-23 13:55:15 -0400

    Actually make the MySQL backup directory

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -2,6 +2,8 @@
 
 currentDate=$(date "+%Y.%m.%d-%H.%M.%S")
 
+mkdir -p $ENDPOINT/${currentDate}/$MYSQL_ENDPOINT
+
 docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB > $ENDPOINT/${currentDate}/$MYSQL_ENDPOINT/backup.sql 2>$ENDPOINT/${currentDate}/$MYSQL_ENDPOINT/errors
 
 chmod 400 $ENDPOINT/${currentDate}/$MYSQL_ENDPOINT/backup.sql
