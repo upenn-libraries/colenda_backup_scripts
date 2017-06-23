d12b909e6c8052fbbbe5252cee8d648cf52dc5f9
Katherine Lynch <katherly@upenn.edu>
2017-06-23 13:38:21 -0400

    Fixed MySQL backup endpoint

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -2,6 +2,6 @@
 
 currentDate=$(date "+%Y.%m.%d-%H.%M.%S")
 
-docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB > /$MYSQL_ENDPOINT/backup_${currentDate}.sql 2>/$ENDPOINT/errors
+docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB > /$MYSQL_ENDPOINT/backup_${currentDate}.sql 2>/$MYSQL_ENDPOINT/errors
 
 chmod 400 /${$MYSQL_ENDPOINT}/backup_${currentDate}.sql
