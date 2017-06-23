07707d76a5d63ba05f572a6bfd922212c0de07dd
Katherine Lynch <katherly@upenn.edu>
2017-06-23 13:40:53 -0400

    Added backup_all script for quickly running a manual backup

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -5,3 +5,4 @@ currentDate=$(date "+%Y.%m.%d-%H.%M.%S")
 docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB > /$MYSQL_ENDPOINT/backup_${currentDate}.sql 2>/$MYSQL_ENDPOINT/errors
 
 chmod 400 /${$MYSQL_ENDPOINT}/backup_${currentDate}.sql
+
