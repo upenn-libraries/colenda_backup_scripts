9a8eefb91b8c3b671d6bb7008191db4ed4793fcd
Katherine Lynch <katherly@upenn.edu>
2017-06-23 14:36:43 -0400

    Modified restory_mysql

--- restore_mysql.sh
+++ restore_mysql.sh
@@ -1,5 +1,12 @@
+#!/usr/bin/bash
 
+if (( $# != 1 ))
+then
+  echo "Usage: Supply the absolute path to the MySQL dump you wish to use"
+  exit 1
+fi
 
+MYSQL_ORIGIN="$1"
 
-cat backup.sql | docker exec -i $MYSQL_CONTAINER /usr/bin/mysql -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB
+cat $MYSQL_ORIGIN | docker exec -i $MYSQL_CONTAINER /usr/bin/mysql -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB
 
