afe839269902a775ce4a1dfd08534b10b8a729f4
Katherine Lynch <katherly@upenn.edu>
2017-06-23 14:26:49 -0400

    WIP added restore_repos script

---
+++ restore_repos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+if (( $# != 1 ))
+then
+  echo "Usage: Supply the absolute path to the backups location you wish to use"
+  exit 1
+fi
+
+$BACKUPS_ORIGIN="$1"
+
+rsync -rlptv $BACKUPS_ORIGIN/ $BARE_REPOS_ORIGIN/
